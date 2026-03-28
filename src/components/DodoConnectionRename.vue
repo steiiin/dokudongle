7bde9fb5068945684368a5ef098b7d59ac330864
dev@steiiin.de <dev@steiiin.de>
2026-03-28 16:34:50 +0100

    fix: strip values

--- src/components/DodoConnectionRename.vue
+++ src/components/DodoConnectionRename.vue
@@ -13,7 +13,7 @@
           <IonButton @click="isModalOpen = false" :disabled="isRenaming">Abbrechen</IonButton>
         </IonButtons>
         <IonButtons slot="end">
-          <IonButton @click="renameDongle" color="success" :disabled="isRenaming">Speichern</IonButton>
+          <IonButton @click="renameDongle" color="success" :disabled="!canSave">Speichern</IonButton>
         </IonButtons>
       </IonToolbar>
     </IonHeader>
@@ -21,7 +21,7 @@
       <IonList lines="none" v-if="!isRenaming">
         <IonItem>
           <IonInput class="dd-input-donglename" ref="nameRef" v-model="newName" :counter="true" :maxlength="20"
-            label="Name" :placeholder="currentName">
+            label="Name" :placeholder="currentName" @ionInput="handleNameInput">
           </IonInput>
         </IonItem>
       </IonList>
@@ -45,18 +45,33 @@ const newName = ref('')
 const nameRef = ref<any|null>(null)
 
 const isRenaming = computed(() => store.connection.isRenaming)
+const canSave = computed(() => !isRenaming.value && newName.value.length > 0)
+
+const sanitizeName = (value: string) => value.replace(/[^a-zA-Z0-9]/g, '')
+
+const setNativeValue = (inputRef: any, value: string) => {
+  const inputCmp = inputRef?.value
+  if (!inputCmp?.$el) return
+  inputCmp.$el.value = value
+}
+const handleNameInput = (event: CustomEvent) => {
+  const inputValue = String(event.detail?.value ?? '')
+  newName.value = sanitizeName(inputValue)
+  setNativeValue(nameRef, newName.value)
+}
 
 const openRenameDialog = async () => {
 
   const baseName = store.connectedDongleName.replace('DokuDongle-', '')
   currentName.value = baseName
-  newName.value = baseName
+  newName.value = sanitizeName(baseName)
   isModalOpen.value = true
   gainFocus(nameRef, true)
 
 }
 
 const renameDongle = async () => {
+  if (!canSave.value) return
   await store.renameDongle(newName.value)
   isModalOpen.value = false
 }
