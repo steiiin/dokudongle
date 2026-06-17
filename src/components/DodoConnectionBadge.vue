60b695812f6fa329db8b3ae89a2c6e5f5e779ae3
dev@steiiin.de <dev@steiiin.de>
2026-06-17 11:34:52 +0200

    fix: new badge icons

--- src/components/DodoConnectionBadge.vue
+++ src/components/DodoConnectionBadge.vue
@@ -2,14 +2,17 @@
   <IonIcon v-if="store.connection.isConnected" class="badge"
     :src="checkmarkCircleOutline" color="success">
   </IonIcon>
-  <IonIcon v-else class="badge"
+  <IonIcon v-else-if="store.connection.isConnecting" class="badge"
     :src="ellipsisHorizontalCircleOutline">
+  </IonIcon>
+  <IonIcon v-else class="badge"
+    :src="removeCircleOutline">
   </IonIcon>
 </template>
 
 <script setup lang="ts">
 
-import { checkmarkCircleOutline, ellipsisHorizontalCircleOutline } from 'ionicons/icons'
+import { checkmarkCircleOutline, ellipsisHorizontalCircleOutline, removeCircleOutline } from 'ionicons/icons'
 
 import { useDokuStore } from '@/store/doku'
 const store = useDokuStore()
